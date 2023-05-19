0ed7d52e0710d48e08f8f3e4aa1fcc5accd2f6fd
Sorekjen <sorenkjellerup@gmail.com>
2023-05-19 16:26:07 +0200

    a

--- src/routes/Toolbox.jsx
+++ src/routes/Toolbox.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { data } from '../data/user.js';
-
 export default function() {
 	return (
-		<div className="flex md:flex-row justify-between md:m-auto p-10 flex-col m-5 text-slate-300">
+		{/*}<div className="flex md:flex-row justify-between md:m-auto p-10 flex-col m-5 text-slate-300">
 			{data.skills && data.skills.map((entry) => (
 				<div className="flex flex-col mb-10">
 					<div className='justify-center text-center w-full font-bold text-lg'>{entry.title}</div>
@@ -13,6 +11,6 @@ export default function() {
 				</div>
 			))}
 		</div>
-	);
+                    */});
 }
 ;
