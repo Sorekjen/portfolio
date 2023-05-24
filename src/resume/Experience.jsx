620d7dfb8304ff5225fc728a771417ce9b03e66d
Jakob Kjellerup <sorenkjellerup@gmail.com>
2023-05-24 19:56:05 +0200

    v1

--- src/resume/Experience.jsx
+++ src/resume/Experience.jsx
@@ -13,12 +13,12 @@ export default function ({ experience}) {
             
 
             <div className=' bg-blue-500 w-fit rounded mx-auto mt-4'>
-            <a href = {"/projects"}>
+            <Link to={"/projects"}>
             <button
                 class=' z-10 font-medium hover:transition hover:-translate-x-1 hover:-translate-y-1 text-blue-500 border-2 p-2 border-blue-500 rounded bg-slate-900'>
                 {experience.button}
             </button>
-            </a>
+            </Link>
             </div>
             <Break />
 
