eb53f93a0fe9ecd0351fe4f1c7b6e39f7b13929f
Jakob Kjellerup <sorenkjellerup@gmail.com>
2023-05-24 19:58:40 +0200

    v1

--- src/resume/Experience.jsx
+++ src/resume/Experience.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Heading from '../common/Heading';
 import Break from '../common/Break';
 import Text from '../common/Text';
+import { Link } from 'react-router-dom';
 
 export default function ({ experience}) {
     return (
